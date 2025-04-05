53f25e13de3e18fbd1b0a7bff45d41d35e9fc0eb
Apollos1301 <abtin@posteo.de>
2025-04-06 00:54:49 +0200

    distributed dev

--- train_eval.py
+++ train_eval.py
@@ -231,6 +231,9 @@ def train_eval_model(model, criterion, optimizer, dataloader, max_norm, num_epoc
                 loss = criterion(similarity_scores, y_values_, s_points, t_points, similarity_scores_2, y_values_2) #, prototype_score
                 loss = loss + layer_loss
                 loss.backward()
+                for param in model.parameters():
+                    dist.all_reduce(param.grad, op=dist.ReduceOp.AVG)
+
                 
                 if max_norm > 0:
                     for name, param in model.named_parameters():
@@ -241,7 +244,7 @@ def train_eval_model(model, criterion, optimizer, dataloader, max_norm, num_epoc
                 optimizer.step()
 
                 
-                model.module.enforce_constraints()
+                model.enforce_constraints()
                 
                 
             with torch.no_grad():
@@ -347,15 +350,21 @@ if __name__ == "__main__":
     # print('Using config file from: ', os.sys.argv[1])
     cfg = update_params_from_cmdline(default_params=cfg)
     
+    device = torch.device("cuda", int(os.environ["LOCAL_RANK"]))
+    torch.cuda.set_device(device)
+    world_size = int(os.environ.get('WORLD_SIZE', 1))
+    local_rank = int(os.environ['LOCAL_RANK']) 
+    output_rank = 0
+    
     #windows
     # dist.init_process_group(backend='gloo', init_method='env://')
     
     #linux
-    dist.init_process_group(backend='nccl', init_method='env://', timeout=timedelta(minutes=60))
+    dist.init_process_group(backend='nccl', rank=local_rank, init_method='env://', timeout=timedelta(minutes=60))
     
-    world_size = int(os.environ.get('WORLD_SIZE', 1))
-    local_rank = int(os.environ['LOCAL_RANK']) 
-    output_rank = 0
+    dist.barrier()
+    
+    
     
     import json
     import os
@@ -388,37 +397,38 @@ if __name__ == "__main__":
     torch.cuda.manual_seed_all(cfg.RANDOM_SEED)
     torch.backends.cudnn.deterministic = True
     
-    dataset_len = {"train": cfg.TRAIN.EPOCH_ITERS * cfg.BATCH_SIZE, "test": cfg.EVAL.SAMPLES * world_size} # 
+    per_gpu_batch_size = cfg.BATCH_SIZE // world_size
+    
+    dataset_len = {"train": cfg.TRAIN.EPOCH_ITERS * per_gpu_batch_size, "test": cfg.EVAL.SAMPLES * world_size} # 
     image_dataset = {
         x: GMDataset(x, cfg.DATASET_NAME, sets=x, length=dataset_len[x], obj_resize=(384, 384)) for x in ("train", "test")
     }
     
     sampler = {
-    "train": DistributedSampler(image_dataset["train"]),
-    "test": DistributedSampler(image_dataset["test"])
+    "train": DistributedSampler(image_dataset["train"], shuffle=True),
+    "test": DistributedSampler(image_dataset["test"], shuffle=True)
     }
     
     dataloader = {x: get_dataloader(image_dataset[x],sampler[x], fix_seed=(x == "test")) for x in ("train", "test")}
 
-    model = NMT()
+    model = NMT().to(device)
         
-    
-    torch.cuda.set_device(local_rank)
-    device = torch.device(f'cuda:{local_rank}')
-    
-    model = model.to(device)
-    model = DDP(model, device_ids=[local_rank], find_unused_parameters=True)
+    #model = DDP(model, device_ids=[local_rank], find_unused_parameters=True)
+    for param in model.parameters():
+        dist.broadcast(param.detach(), 0)
 
     # criterion = torch.nn.CrossEntropyLoss()
     criterion = InfoNCE_Loss(temperature=cfg.TRAIN.temperature)
-    backbone_params = model.module.backbone_params
+    backbone_params = model.backbone_params
 
     backbone_ids = [id(item) for item in backbone_params]
 
     new_params = [param for param in model.parameters() if id(param) not in backbone_ids]
+    
+    scaled_LR = cfg.TRAIN.LR * math.sqrt(world_size)
     opt_params = [
-        dict(params=backbone_params, lr=cfg.TRAIN.LR * 0.03),
-        dict(params=new_params, lr=cfg.TRAIN.LR),
+        dict(params=backbone_params, lr=scaled_LR * 0.03),
+        dict(params=new_params, lr=scaled_LR),
     ]
     optimizer = optim.Adam(opt_params, weight_decay=cfg.TRAIN.weight_decay)
     
